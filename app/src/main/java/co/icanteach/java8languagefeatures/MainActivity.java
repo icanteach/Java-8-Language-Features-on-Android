f2bb5e38a1ff68ee75fa8864c19f083e52fd98d6
Murat Can BUR <muratcanbur@gmail.com>
2016-12-02 23:37:22 +0200

    first commit

---
+++ app/src/main/java/co/icanteach/java8languagefeatures/MainActivity.java
@@ -0,0 +1,19 @@
+package co.icanteach.java8languagefeatures;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
